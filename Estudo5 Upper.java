79916cb731090c0109d44443991497af85f6ebbc
Andrew <50187249+NDRandrew@users.noreply.github.com>
2022-06-24 13:19:38 -0300

    Update and rename Estudo5.java to Estudo5 Upper.java

--- Estudo5 Upper.java
+++ Estudo5 Upper.java
@@ -10,24 +10,24 @@ public class Estudo5 {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            var initialText = "¿Hablas español o inglés o ambos?";
+            var initialText = "Olá meu nome é programa!"; //O que será utilizado como conteudo para o codigo
             var area = new JTextArea(initialText, 8, 50);
 
-            var lowerCaseButton = new JButton("To Lower Case");
+            var lowerCaseButton = new JButton("To Lower Case"); //Escrita no botao
             var upperCaseButton = new JButton("To Upper Case");
-            lowerCaseButton.addActionListener(e -> area.setText(area.getText().toLowerCase()));
-            upperCaseButton.addActionListener(e -> area.setText(area.getText().toUpperCase()));
+            lowerCaseButton.addActionListener(e -> area.setText(area.getText().toLowerCase())); //Botao para diminuir as letras (caixa baixa)
+            upperCaseButton.addActionListener(e -> area.setText(area.getText().toUpperCase())); //Botao para aumentar as letras (caixa alta)
 
             var buttonPanel = new JPanel();
-            buttonPanel.add(lowerCaseButton);
+            buttonPanel.add(lowerCaseButton); //Adicionando os Botoes
             buttonPanel.add(upperCaseButton);
-
+                        //Parte Grafica
             var frame = new JFrame("Capitalizer");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.getContentPane().add(new JScrollPane(area), BorderLayout.CENTER);
             frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
             frame.pack();
             frame.setVisible(true);
-        });
+        }); //Código com interface gráfica simples com botões, necessita de polimento, baseado no código do Imu.edu
     }
 }
